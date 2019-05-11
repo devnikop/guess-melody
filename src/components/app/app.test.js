614dc16b1c5734495c781df92f8b9bf2de371ad2
devnikop <petrov.petrov.ak@gmail.com>
2019-05-11 18:22:55 +0300

    add snapshot test for app

---
+++ src/components/app/app.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {App} from './app.jsx';
+
+it(`App correctly renders`, () => {
+  const tree = renderer
+    .create(<App
+      gameTime={0}
+      errorCount={0}
+      onWelcomeButtonClick={jest.fn()}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
