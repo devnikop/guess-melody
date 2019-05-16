42746c33429a095f218957e29a62eac4afbfe85e
devnikop <petrov.petrov.ak@gmail.com>
2019-05-16 16:40:19 +0300

    add e2e test for GenreQuestionScreen

---
+++ src/components/genre-question-screen/genre-question-screen.e2e.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {GenreQuestionScreen} from './genre-question-screen.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const answers = [
+  {
+    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+    genre: `rock`,
+  },
+  {
+    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+    genre: `pop`,
+  },
+  {
+    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+    genre: `jazz`,
+  },
+  {
+    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+    genre: `rock`,
+  },
+];
+
+it(`GenreQuestionScreen's form submit`, () => {
+  const formSubmit = jest.fn();
+  const genreQuestionScreen = shallow(<GenreQuestionScreen
+    genre={`rock`}
+    answers={answers}
+    onAnswer={formSubmit}
+  />);
+
+  const submitButton = genreQuestionScreen.find(`.game__tracks`);
+  submitButton.simulate(`submit`, {
+    preventDefault: () => {}
+  });
+
+  expect(formSubmit).toHaveBeenCalledTimes(1);
+});
