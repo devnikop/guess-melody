fcbea649f3d39da9f1a7ec1d94c711883233acc8
devnikop <petrov.petrov.ak@gmail.com>
2019-05-16 16:56:20 +0300

    add e2e test for ArtistQuestionScreen

---
+++ src/components/artist-question-screen/artist-question-screen.e2e.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {ArtistQuestionScreen} from './artist-question-screen.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const song = {
+  artist: `Jim Beam`,
+  src: `path.mp3`,
+};
+
+const answers = [
+  {
+    picture: `path.jpg`,
+    artist: `John Snow`,
+  },
+  {
+    picture: `path.jpg`,
+    artist: `Jack Daniels`,
+  },
+  {
+    picture: `path.jpg`,
+    artist: `Jim Beam`,
+  },
+];
+
+it(`ArtistQuestionScreen's form submit`, () => {
+  const formSubmit = jest.fn();
+  const artistQuestionScreen = shallow(<ArtistQuestionScreen
+    song={song}
+    answers={answers}
+    onAnswer={formSubmit}
+  />);
+
+  const submitButton = artistQuestionScreen.find(`.game__artist`);
+  submitButton.simulate(`change`);
+
+  expect(formSubmit).toHaveBeenCalledTimes(1);
+});
