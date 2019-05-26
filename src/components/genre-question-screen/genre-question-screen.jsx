28e880ba79b2abd4c6f7f0209dec841e1fea8589
devnikop <petrov.petrov.ak@gmail.com>
2019-05-26 23:59:13 +0300

    update artist&genre screens

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -7,75 +7,71 @@ export class GenreQuestionScreen extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    const {answers} = this.props.question;
+
     this.state = {
       activePlayer: -1,
+      userAnswer: new Array(answers.length).fill(false),
     };
   }
 
   render() {
     const {
-      genre,
-      answers,
+      question,
       onAnswer,
     } = this.props;
+    const {
+      genre,
+      answers,
+    } = question;
 
-    return <section className="game game--genre">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
+    return <section className="game__screen">
+      <h2 className="game__title">Выберите {genre} треки</h2>
+      <form className="game__tracks" onSubmit={(evt) => {
+        evt.preventDefault();
+        onAnswer(this.state.userAnswer);
+      }}>
+        {answers.map((it, i) => <div className="track" key={`answer-${i}`}>
+          <AudioPlayer
+            isPlaying={i === this.state.activePlayer}
+            onPlayButtonClick={() =>
+              this.setState({
+                activePlayer: this.state.activePlayer === i ? -1 : i
+              })}
+            src={it.src}
+          />
+          <div className="game__answer">
+            <input
+              className="game__input visually-hidden"
+              type="checkbox" name="answer"
+              value={`answer-${i}`}
+              id={`answer-${i}`}
+              onChange={() => {
+                const userAnswer = [...this.state.userAnswer];
+                userAnswer[i] = !userAnswer[i];
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
-        </svg>
-
-        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-          <span className="timer__mins">05</span>
-          <span className="timer__dots">:</span>
-          <span className="timer__secs">00</span>
-        </div>
-
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
-
-      <section className="game__screen">
-        <h2 className="game__title">Выберите {genre} треки</h2>
-        <form className="game__tracks" onSubmit={(evt) => {
-          evt.preventDefault();
-          onAnswer();
-        }}>
-          {answers.map((it, i) => <div className="track" key={`answer-${i}`}>
-            <AudioPlayer
-              isPlaying={i === this.state.activePlayer}
-              onPlayButtonClick={() =>
                 this.setState({
-                  activePlayer: this.state.activePlayer === i ? -1 : i
-                })}
-              src={it.src}
+                  userAnswer,
+                });
+              }}
             />
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`} id={`answer-${i}`} />
-              <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
-            </div>
-          </div>)}
-          <button className="game__submit button" type="submit">Ответить</button>
-        </form>
-      </section>
+            <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
+          </div>
+        </div>)}
+        <button className="game__submit button" type="submit">Ответить</button>
+      </form>
     </section>;
   }
 }
 
 GenreQuestionScreen.propTypes = {
-  genre: propTypes.oneOf([`rock`, `pop`, `jazz`]).isRequired,
-  answers: propTypes.arrayOf(propTypes.shape({
+  question: propTypes.shape({
+    type: propTypes.oneOf([`genre`]).isRequired,
     genre: propTypes.oneOf([`rock`, `pop`, `jazz`]).isRequired,
-    src: propTypes.string.isRequired,
-  })).isRequired,
+    answers: propTypes.arrayOf(propTypes.shape({
+      genre: propTypes.oneOf([`rock`, `pop`, `jazz`]).isRequired,
+      src: propTypes.string.isRequired,
+    })).isRequired,
+  }),
   onAnswer: propTypes.func,
 };
--- src/components/genre-question-screen/genre-question-screen.test.js
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -4,37 +4,38 @@ import renderer from 'react-test-renderer';
 import {GenreQuestionScreen} from './genre-question-screen.jsx';
 
 const mock = {
-  genre: `rock`,
-  answers: [
-    {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-      genre: `rock`,
-    },
-    {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-      genre: `pop`,
-    },
-    {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-      genre: `jazz`,
-    },
-    {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-      genre: `rock`,
-    },
-  ],
+  question: {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `pop`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+  }
 };
 
 it(`GenreQuestionScreen correctly renders`, () => {
   const {
-    genre,
-    answers,
+    question
   } = mock;
 
   const tree = renderer
     .create(<GenreQuestionScreen
-      genre={genre}
-      answers={answers}
+      question={question}
       onAnswer={jest.fn()}
     />)
     .toJSON();
