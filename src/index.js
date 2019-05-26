686885ad4f202ca63c55f4ef932e8b01b13d1ec7
devnikop <petrov.petrov.ak@gmail.com>
2019-05-26 23:56:59 +0300

    update index.js for redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-import {App} from './components/app/app.jsx';
+import App from './components/app/app.jsx';
 import {questions} from './mocks/questions';
+import {reducer} from './reducer';
+
+const gameSettings = {
+  gameTime: 5,
+  errorCount: 3,
+};
 
 const init = (gameQuestions) => {
-  const settings = {
-    gameTime: 5,
-    errorCount: 3,
-    onWelcomeButtonClick: () => {},
-  };
+  const {errorCount, gameTime} = gameSettings;
+  const store = createStore(reducer);
+
   ReactDOM.render(
-      <App
-        errorCount={settings.errorCount}
-        gameTime={settings.gameTime}
-        questions={gameQuestions}
-      />,
+      <Provider store={store}>
+        <App
+          maxMistakes={errorCount}
+          gameTime={gameTime}
+          questions={gameQuestions}
+        />
+      </Provider>,
       document.querySelector(`.main`)
   );
 };
