242006c43760a006a5feac269c6a29d87847369d
devnikop <petrov.petrov.ak@gmail.com>
2020-04-22 13:49:51 +0300

    add setup for enzyme

--- src/components/welcome-screen/__tests__/welcome-screen.e2e.test.jsx
+++ src/components/welcome-screen/__tests__/welcome-screen.e2e.test.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import WelcomeScreen from "../welcome-screen.jsx";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it(`Click on button correctly works`, () => {
   const clickHandler = jest.fn();
