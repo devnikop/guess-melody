bf0dd26b1653f65fa7717f6676b71f050f632cd2
devnikop <petrov.petrov.ak@gmail.com>
2020-04-23 15:11:08 +0300

    add tests for state

--- src/components/app/__tests__/app.test.jsx
+++ src/components/app/__tests__/app.test.jsx
@@ -3,9 +3,59 @@ import renderer from "react-test-renderer";
 
 import App from "../app.jsx";
 
+const mock = {
+  questions: [
+    {
+      answers: [
+        {
+          genre: `rock`,
+          src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        },
+        {
+          genre: `pop`,
+          src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        },
+        {
+          genre: `jazz`,
+          src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        },
+        {
+          genre: `rock`,
+          src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        },
+      ],
+      genre: `rock`,
+      type: `genre`,
+    },
+    {
+      answers: [
+        {
+          picture: `path.jpg`,
+          artist: `John Snow`,
+        },
+        {
+          picture: `path.jpg`,
+          artist: `Jack Daniels`,
+        },
+        {
+          picture: `path.jpg`,
+          artist: `Jim Beam`,
+        },
+      ],
+      song: {
+        artist: `Jim Beam`,
+        src: `path.mp3`,
+      },
+      type: `artist`,
+    },
+  ],
+};
+
 it(`snapshot`, () => {
+  const { questions } = mock;
+
   const tree = renderer
-    .create(<App errorCount={5} gameTime={3} questions={[]} />)
+    .create(<App errorCount={5} gameTime={3} questions={questions} />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
