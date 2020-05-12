fd623804e5fe924223f728c26785eaa83665a421
devnikop <petrov.petrov.ak@gmail.com>
2020-05-12 18:07:13 +0300

    add snapshot test for component Mistakes

---
+++ src/components/mistakes/__tests__/mistakes.test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import {Mistakes} from "../mistakes.jsx";
+
+it(`snapshot`, () => {
+  const tree = renderer.create(<Mistakes mistakes={2}/>);
+
+  expect(tree).toMatchSnapshot();
+});
