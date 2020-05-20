a3959ca70c7f7c96d5d4b98208a9aaf39732a636
devnikop <petrov.petrov.ak@gmail.com>
2020-05-20 16:11:31 +0300

    add test for hoc with-answers

---
+++ src/hocs/with-answers/__tests__/with-answers.e2e.test.jsx
@@ -0,0 +1,54 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+import withAnswers from "../with-answers.jsx";
+
+const mock = {
+  question: {
+    answers: [
+      {
+        genre: `rock`,
+        src: `source1`,
+      },
+      {
+        genre: `jazz`,
+        src: `source2`,
+      },
+    ],
+    genre: `rock`,
+    type: `genre`,
+  },
+};
+
+describe(`state answers`, () => {
+  let onInputChange;
+  let getAnswers;
+
+  beforeEach(() => {
+    const { question } = mock;
+    const MockComponent = () => <div />;
+    const MockComponentWrapped = withAnswers(MockComponent);
+    const wrapper = shallow(<MockComponentWrapped
+      question={question}
+    />);
+
+    onInputChange = wrapper.props().onInputChange;
+    getAnswers = () => wrapper.state().answers;
+  });
+
+  it(`default value equal [false, false]`, () => {
+    expect(getAnswers()).toEqual([false, false]);
+  });
+
+  it(`change to [true, false] when call onInputChange(0)`, () => {
+    onInputChange(0);
+    expect(getAnswers()).toEqual([true, false]);
+  });
+
+  it(`change to [false, true] && [false, false] when call onInputChange(1) twice`, () => {
+    onInputChange(1);
+    expect(getAnswers()).toEqual([false, true]);
+    onInputChange(1);
+    expect(getAnswers()).toEqual([false, false]);
+  });
+});
